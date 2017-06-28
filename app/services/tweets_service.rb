ce8fef2d84f15389126887b89b5ad6087ef05ef0
Tiago Tiede <tiago@navut.com>
2017-06-28 13:11:07 -0300

    [Services][DEV] Method to Request

--- app/services/tweets_service.rb
+++ app/services/tweets_service.rb
@@ -1,10 +1,22 @@
 # Service to call endpoint list tweets
 require 'net/http'
 class TweetsService
+  
+  @uri = URI('http://tweeps.locaweb.com.br/tweeps')
+  
   def list
-    uri = URI('http://tweeps.locaweb.com.br/tweeps')
-    request = Net::HTTP::Get.new uri.request_uri
+    request =  resquest_tweets_get
 
-    http = Net::HTTP.new(uri.host, uri.port)
+    http = Net::HTTP.new(@uri.host, @uri.port)
+    request.initialize_http_header('Username' => 'geschopf@gmail.com')
+    http.start
+    response = http.request(request)
+    return JSON.parse(response.body)
+  rescue StandardError => e
+    Rails.logger.fatal "Error: TweetsService: #{e}"
+  end
+
+  def resquest_tweets_get
+    Net::HTTP::Get.new @uri.request_uri
   end
 end
