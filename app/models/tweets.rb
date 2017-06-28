6c00bc9aeea392164676b605711d28fe28c84ff8
Tiago Tiede <tiago@navut.com>
2017-06-28 14:27:23 -0300

    [Model][DEV] Tweets: Refactored Method Filter

--- app/models/tweets.rb
+++ app/models/tweets.rb
@@ -1,6 +1,6 @@
 # List of tweets and methods to filters
 class Tweets
-  attr_reader :all_tweets
+  attr_reader :all_tweets, :valid_tweets
 
   def initialize
     @all_tweets = TweetsService.new.list
@@ -8,10 +8,14 @@ class Tweets
 
   def filter
     @all_tweets['statuses'].each do |tweet|
-      tweet['entities']['user_mentions'].each do |mention|
-      	next unless mention['id'] == 42
-      	@valid_tweets << tweet(tweet) unless tweet['in_reply_to_user_id'] == 42
-      end
+      list_tweets(tweet)
+    end
+  end
+
+  def list_tweets(tweet)
+    tweet['entities']['user_mentions'].each do |mention|
+      next unless mention['id'] == 42
+      @valid_tweets << tweet(tweet) unless tweet['in_reply_to_user_id'] == 42
     end
   end
 
