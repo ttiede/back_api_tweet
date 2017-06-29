716e253f97d4faed16f47bea4ad94a8c9e5aabf1
Tiago Tiede <tiago@navut.com>
2017-06-29 16:45:30 -0300

    [Service][BUG] Fix method error

--- app/services/tweets_service.rb
+++ app/services/tweets_service.rb
@@ -1,22 +1,16 @@
 # Service to call endpoint list tweets
 require 'net/http'
 class TweetsService
-  
-  @uri = URI('http://tweeps.locaweb.com.br/tweeps')
-  
   def list
-    request =  resquest_tweets_get
+    uri = URI('http://tweeps.locaweb.com.br/tweeps')
+    request = Net::HTTP::Get.new uri.request_uri
 
-    http = Net::HTTP.new(@uri.host, @uri.port)
+    http = Net::HTTP.new(uri.host, uri.port)
     request.initialize_http_header('Username' => 'geschopf@gmail.com')
     http.start
     response = http.request(request)
     return JSON.parse(response.body)
   rescue StandardError => e
     Rails.logger.fatal "Error: TweetsService: #{e}"
-  end
-
-  def resquest_tweets_get
-    Net::HTTP::Get.new @uri.request_uri
   end
 end
