e1790a8447139873c0e1f0f7f017ff11e224a932
Tiago Tiede <tiago@navut.com>
2017-06-29 09:32:24 -0300

    [Model][DEV] Tweet with all attributes

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet
-  attr_accessor :screen_name
+  attr_accessor :screen_name, :followers_count, :retweet_count, :favourites_count, 
+  				:text, :created_at, :user_url, :tweet_url
 
   def initialize(attributes = {})
     attributes.each do |name, value|
